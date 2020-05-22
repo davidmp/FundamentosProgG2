dbd283c484a7d7c322c0693dd6c64064b83df464
davidmp <mamanipari@gmail.com>
2020-05-22 13:23:23 -0500

    Repaso

---
+++ Examen01.psc
@@ -0,0 +1,20 @@
+Algoritmo CalcularBonoDMP
+	Definir puntos Como Entero
+	Definir salariominimo, montobono  Como Real
+	//datos de entrada
+	Escribir "Ingrese el Salario Minimo:"
+	Leer salariominimo
+	Escribir "Ingrese el Puntaje que ha obtenido el Profesor:"
+	Leer puntos
+	//Proceso
+	si puntos>=0 y puntos<=100 Entonces
+			montobono=1*salariominimo
+	sino si puntos>=101 y puntos<=150 Entonces
+			montobono=2*salariominimo
+		SiNo
+			montobono=3*salariominimo
+		FinSi		
+	FinSi
+	//Datos de salida
+	Escribir "El monto de bono es:", montobono
+FinAlgoritmo
---
+++ RepasoExamen.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class RepasoExamen{
+
+   static Scanner teclado=new Scanner(System.in);
+
+    public static void calcularBonoDMP(){
+        int puntos;
+        double salariominimo, montobono;
+        //datos de entrada
+        System.out.println("Ingrese el Salario Minimo:");
+        salariominimo=teclado.nextDouble();
+        System.out.println("Ingrese el Puntaje que ha obtenido el Profesor:");
+        puntos=teclado.nextInt();       
+        //Proceso
+        if(puntos>=0 && puntos<=100){
+            montobono=1*salariominimo;
+        }else if(puntos>=101 && puntos<=150){
+            montobono=2*salariominimo;
+        }else{
+            montobono=3*salariominimo;
+        }
+        //Datos de salida
+        System.out.println("El monto de bono es:"+montobono);
+
+    }
+
+    public static void main(String [] arg){
+        System.out.println("Holas");
+        calcularBonoDMP();
+    }
+
+
+}
