b07436a885ce9cdcfefdea1a20c417d923d96c30
davidmp <mamanipari@gmail.com>
2020-05-25 10:18:04 -0500

    Operaciones con tipo de datos

--- codexample/sesion06/Sesion06.java
+++ codexample/sesion06/Sesion06.java
@@ -1,5 +1,36 @@
 package codexample.sesion06;
 
 public class Sesion06{
+    
+    public static void main(String[] args) {
+        byte pppp=30, repp;
+
+   
+
+        System.out.println("Resultado:"+pr);        
+        //Numericos
+        short pa=32, pb=2;
+        int pr=0;
+        pr=(int)pa*pb;
+
+        System.out.println("Resultado:"+pr);
+        int a=90000, b=90000, re;
+        re=a*b;
+        System.out.println("Resultado:"+re);
+        
+        long al=900000000, bl=900000000, rel;
+        rel=al*bl;
+        System.out.println("Resultado:"+rel);
+
+        float x, y;
+        
+        
+        double m, n;
+        //Cadenas
+        char l=' ';
+        String nombre="";
+        //boleanos
+        boolean condicion=false;
+    }
 
 }
