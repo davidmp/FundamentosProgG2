656c30183f74ddb54b65d49ff5e1f4312b8d32c8
repl.it user <replituser@example.com>
2020-06-01 13:31:51 +0000

    Solucionar problemas git

---
+++ codexample/EjerciciosCiclicos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+class EjerciciosCiclicos{
+  public:
+  void calcularFactorialNum(){
+     printf("Hola Mundo\n");
+  }
+};
+//g++ DemoC.cpp -o PruebaC
+//./PruebaC 
+int main() {
+  EjerciciosCiclicos obj;
+  obj.calcularFactorialNum();
+return 0; 
+}
