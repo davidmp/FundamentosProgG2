d3776df07a349b49e23bff8eb2b0f316d9f887bd
davidmp <mamanipari@gmail.com>
2020-06-05 11:39:19 -0500

    Ejercicio 4.11

--- codexample/EjerciciosCiclicos.java
+++ codexample/EjerciciosCiclicos.java
@@ -22,9 +22,38 @@ public class EjerciciosCiclicos{
     System.out.println("El Factorial de "+numero+" Es:"+resultado);
   }
 
+  //Ejercicio 4.11
+  public static long numFactorial(int numero){//4
+      int contador=1;
+      long resultado=1;
+      if(numero>0){
+      while(contador<numero){
+                contador++;    //4          
+                resultado=resultado*contador; //24          
+              }
+      }    
+      return resultado;
+  }
+
+  public static void calcularFuncionExponencial(){
+    int numeroX;
+    double resultadoFE=1;
+    //Datos de Entrada
+    System.out.println("Ingrese el Numero X de FE:");
+    numeroX=teclado.nextInt();
+    for(int contador=1;contador<=numeroX;contador++){
+      resultadoFE+=Math.pow(numeroX, contador)/numFactorial(contador);
+    }
+    System.out.println("La funcion exponencial del numero "+numeroX+" es:"+resultadoFE);
+  }
+
+
+
   public static void main(String[] ars){
     System.out.println("Holas");
-    calcularFactorialNum();
+    //calcularFactorialNum();
+    calcularFuncionExponencial();
+
   }
 
 }
