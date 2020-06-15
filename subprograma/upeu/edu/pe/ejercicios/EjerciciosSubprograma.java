8aa741a342018b80b6029bc9f02be6f17e17cfdb
davidmp <mamanipari@gmail.com>
2020-06-15 10:27:09 -0500

    Ejemplos recursivos

---
+++ subprograma/upeu/edu/pe/ejercicios/EjerciciosSubprograma.java
@@ -0,0 +1,43 @@
+package upeu.edu.pe.ejercicios;
+
+import java.math.BigInteger;
+
+public class EjerciciosSubprograma {
+    //6.2
+    public BigInteger numFactorial(int numero){
+        int contador=1;
+        BigInteger resultado=BigInteger.valueOf(1);
+        if(numero>0){
+        while(contador<numero){
+                  contador++;              
+                  resultado=resultado.multiply(BigInteger.valueOf(contador));        
+                }
+        }    
+        return resultado;
+    }
+    public void factorialRango2Numeros(int numInicial, int numFinal){
+        for(int numInit=numInicial; numInit<=numFinal; numInit++){
+            BigInteger resultado=numFactorial(numInit);
+            System.out.println("El Factorial de "+numInit+" es:"+resultado);
+        }
+    }
+
+    public BigInteger numFactorialRecur(int numero){
+        BigInteger resultado=BigInteger.valueOf(1);
+        if(numero>0){
+            resultado=(BigInteger.valueOf(numero)).multiply(numFactorialRecur(numero-1));  
+        }
+        return resultado;
+    }
+
+    public int fibonaciRec(int n){
+        if(n<0){ return -1; }
+        if(n==0 || n==1){ return n; }
+        else{
+            return fibonaciRec(n-1)+fibonaciRec(n-2);
+        }
+    }
+
+
+
+}
