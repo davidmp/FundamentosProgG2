2b5a6ed05aac146a9287271297e0d530e53eac03
davidmp <mamanipari@gmail.com>
2020-06-30 17:36:35 -0500

    Matrices Repaso

--- PyReforzamiento/src/main/java/pe/edu/upeu/reforzamiento/App.java
+++ PyReforzamiento/src/main/java/pe/edu/upeu/reforzamiento/App.java
@@ -1,6 +1,7 @@
 package pe.edu.upeu.reforzamiento;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -11,7 +12,7 @@ import java.awt.*;
 public class App {
     static JFrame freme;
 
-
+    static Scanner leerT=new Scanner(System.in);
 
 
     public static void main( String[] args ) throws IOException {
@@ -22,6 +23,10 @@ public class App {
         freme.setSize(new Dimension(screenSize.width,screenSize.height));
 
         freme.setVisible(true);*/
+       
+        //Vectores
+        JButton[] buttonX=new JButton[6];
+
         String[] nombreVector;        
         nombreVector=new String[19];
         nombreVector[0]="Alejo Castillo Cristhian Brandonlyn";
@@ -31,10 +36,27 @@ public class App {
 
         String vectorX[]={"Alejo Castillo Cristhian Brandonlyn", "Yepez Heredia Diego Leyter", "Ccoto Cruz Jose Israel"};
         vectorX[1]="Ccori Ccoarite Guver Leon";
-        
+
         System.out.println("Tamanho v2:"+vectorX.length);
         for(int indice=0;indice<vectorX.length;indice++){
             System.out.println("vectorX["+indice+"]:="+vectorX[indice]);
+        }
+        
+        //Matrices
+        String[][] data={
+            {"Burgos Pari Diego Armando", ""},
+            {"Callata Amones Michael Jordan", ""},
+            {"Carlos Yucra Wily Abrahan", ""},
+            };
+
+        System.out.println("Tamanho Fila:"+data.length);
+        System.out.println("Tamanho Columna:"+data[0].length);
+
+        System.out.println("Llamar Lista");
+
+        for(int indice=0;indice<data.length;indice++){
+            System.out.println("Llamando lista a "+data[indice][0]);
+            data[indice][1]=leerT.next();
         }
 
     }
