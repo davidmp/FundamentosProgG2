c4ad28f41f2b59df32ad6ca8bd58bd0ba236b1bd
davidmp <mamanipari@gmail.com>
2020-07-03 11:32:54 -0500

    Menu de opciones

--- ProyectoUPeUGUI/src/main/java/pe/edu/upeu/app/App.java
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/app/App.java
@@ -1,5 +1,7 @@
 package pe.edu.upeu.app;
 
+import pe.edu.upeu.utils.LeerTeclado;
+
 /**
  * Hello world!
  *
@@ -7,7 +9,29 @@ package pe.edu.upeu.app;
 public class App 
 {
     public static void main( String[] args ){
-        System.out.println( "Hello World!" );
-        new MainGUI();
+        System.out.println( "Systema de Menu de opciones******" );    
+        try {
+            LeerTeclado teclado=new LeerTeclado();            
+            char opcion='S'; //S=SI, N=NO
+            int numAlgoritm=1;
+            do{            
+                numAlgoritm=teclado.leer(0,
+                "Ingrese el numero de Algoritmo que desea probar: \n"+
+                "01=Entorno Grafico\n"+
+                "02=xxxx \n"+
+                "03=xxx");                
+                switch(numAlgoritm){
+                    case 1: 
+                    new MainGUI();         
+                    break;  
+                    default: System.out.println("La opcion No existe!!"); break;
+                }            
+                opcion=teclado.leer(' ', "Desea Probar mas Algoritmos? S=SI, N=NO");
+            }while(opcion=='S' || opcion=='s');   
+        } catch (Exception er) {
+            System.out.println(er.getMessage());
+        }
+
     }
+
 }
