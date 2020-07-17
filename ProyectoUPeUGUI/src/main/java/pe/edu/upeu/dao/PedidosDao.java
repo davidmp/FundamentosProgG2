51abcefe8c40d79846ef06ebbc1a96cfc82b08c8
davidmp <mamanipari@gmail.com>
2020-07-17 12:54:43 -0500

    Trabajo con fechas y IdX

--- ProyectoUPeUGUI/src/main/java/pe/edu/upeu/dao/PedidosDao.java
+++ ProyectoUPeUGUI/src/main/java/pe/edu/upeu/dao/PedidosDao.java
@@ -1,5 +1,8 @@
 package pe.edu.upeu.dao;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import pe.edu.upeu.modelo.Pedidos;
 import pe.edu.upeu.utils.LeerArchivo;
 import pe.edu.upeu.utils.LeerTeclado;
@@ -8,8 +11,19 @@ public class PedidosDao extends AppCrud{
     LeerArchivo leerArc;
     Pedidos pedTO;
     LeerTeclado teclado=new LeerTeclado();
+    SimpleDateFormat formateador = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
-    public Object[][] registrarPedido(){                    
+    public String generadorId(LeerArchivo leerArcX, int numColum, String prefijo){
+        int idX=1;
+        Object[][] data=listarContenido(leerArcX);
+        if(data!=null){
+            idX=Integer.parseInt(data[data.length-1][numColum].toString().substring(1))+1;
+        }
+        return prefijo+""+idX;
+    }
+
+    public Object[][] registrarPedido(){  
+        System.out.println("****************Registrar Nuevo Pedido*************");                  
         leerArc=new LeerArchivo("Productos.txt");         
         Object[][] listaProd=listarContenido(leerArc);
         for(int i=0;i<listaProd.length;i++){
@@ -17,11 +31,19 @@ public class PedidosDao extends AppCrud{
         }
         System.out.println("");
         pedTO=new Pedidos();
+        leerArc=new LeerArchivo("Pedidos.txt");  
+        pedTO.setPedidoId(generadorId(leerArc, 0, "P"));
+        
         pedTO.setProductoId(teclado.leer("", "Ingrese el Codigo del Producto:"));
         pedTO.setDescripPedido(teclado.leer("", "Ingrese una breve descripcion:"));
         pedTO.setCantidad(teclado.leer(0.0, "Ingrese la cantidad:"));
-        listaProd=buscarContenido(leerArc, 0, pedTO.getProductoId());
+
+        leerArc=new LeerArchivo("Productos.txt");   
+        listaProd=buscarContenido(leerArc, 0, pedTO.getProductoId());        
         pedTO.setPrecioUnit(Double.parseDouble(listaProd[0][2].toString()));
+
+        pedTO.setFechaPed(formateador.format(new Date()));
+
         pedTO.setPrecioTotal(pedTO.getCantidad()*pedTO.getPrecioUnit());        
         leerArc=new LeerArchivo("Pedidos.txt");    
         return agregarContenido(leerArc, pedTO);
